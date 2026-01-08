d517a26ee27f1cb4f68a569851b750aa701bc378
Samrat Srivastava <samrat@pocketful.in>
2026-01-08 14:21:07 +0530

    Added job to load sport-data from api

--- src/main/java/com/example/stapubox/StapuboxApplication.java
+++ src/main/java/com/example/stapubox/StapuboxApplication.java
@@ -3,8 +3,21 @@ package com.example.stapubox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.stapubox.jobs.SportsDataLoader;
+
 @SpringBootApplication
 public class StapuboxApplication {
+
+	private SportsDataLoader sportsDataLoader;
+
+	StapuboxApplication(SportsDataLoader sportsDataLoader) {
+		this.sportsDataLoader = sportsDataLoader;
+		try {
+			sportsDataLoader.loadSportsData();
+		} catch (Exception e) {
+			System.err.println("Failed to load sports data: " + e.getMessage());
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StapuboxApplication.class, args);
